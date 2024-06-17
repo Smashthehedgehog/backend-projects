de6c6df9ce8ac50a4b8e0ab136657849756961c2
Smashthehedgehog <137214872+Smashthehedgehog@users.noreply.github.com>
2024-06-17 12:17:12 -0400

    add

---
+++ RESTful_Web_Service/demo/src/main/java/com/example/demo/Greeting.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+public class Greeting {
+    
+    private Long id;
+    private String context;
+    
+    public Greeting(Long id, String context) {
+        this.id = id;
+        this.context = context;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getContext() {
+        return context;
+    }
+
+}
---
+++ User_Registration_And_Login/user-registration-frontend/src/components/AppContent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import WelcomeContent from './WelcomeContent'
+
+export default class AppContent extends React.Component {
+    render() {
+        return (
+            <div>
+                <WelcomeContent />
+            </div>
+        )
+    }
+}

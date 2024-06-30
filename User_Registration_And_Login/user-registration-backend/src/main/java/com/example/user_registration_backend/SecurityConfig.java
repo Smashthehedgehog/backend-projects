7b03d0df33841ffc8361ceb9dc871439d8ecda93
Smashthehedgehog <137214872+Smashthehedgehog@users.noreply.github.com>
2024-06-30 18:40:55 -0400

    Added security for backend

--- User_Registration_And_Login/user-registration-backend/src/main/java/com/example/user_registration_backend/SecurityConfig.java
+++ User_Registration_And_Login/user-registration-backend/src/main/java/com/example/user_registration_backend/SecurityConfig.java
@@ -3,10 +3,13 @@ package com.example.user_registration_backend;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -32,7 +35,15 @@ public class SecurityConfig {
         return authProvider;
     }
 
-    // Add Authentication Provider somehow
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http, PasswordEncoder passwordEncoder, UserDetailsService userDetailsService)
+            throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder)
+                .and()
+                .build();
+    }
 
     @Bean public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
